20397f4d34030c18f8cc3119af25625d44ec968a
Hiroaki Mitsuyoshi <thnksfrth3244@gmail.com>
2024-04-13 03:17:16 +0900

    #17 --versionの追加

--- mbpstester.py
+++ mbpstester.py
@@ -5,10 +5,13 @@ from modules.result_formatter import format_result
 from modules.write_log import write_log
 from config import DOWNLOAD_URL, UPLOAD_URL, SIZE
 
+VERSION = '0.1.0'
+
 def parse_arguments():
     parser = argparse.ArgumentParser(description='Measure download and upload speeds and optionally log the results.')
     parser.add_argument('--log', action='store_true', help='Enable logging of speed test results')
     parser.add_argument('--log-file', type=str, default='mbpstester.log', help='Specify the path to the log file (default: mbpstester.log)')
+    parser.add_argument('--version', '-v', action='version', version=f'mbpstester version {VERSION}', help='Show the version of the program and exit')
     return parser.parse_args()
 
 def main():
@@ -16,7 +19,7 @@ def main():
 
     if args.log:
         write_log(args.log_file, result)
-        
+
     try:
         downloader = Downloader(DOWNLOAD_URL, SIZE)
         download_speed = downloader.measure_speed()
